a667a3e9cd6e28fc430d495f270bf6565a80b1af
nguyenduc2511 <nguyenduc251196@gmail.com>
2022-01-14 00:37:22 +0700

    front_end_agular

---
+++ -C0721G1_Project_Sprint1_Frontend/c0721g1-medical-supplies/src/app/common/header/header.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    document.querySelector('.infor_link-toggle')
+      .addEventListener('click', classToggle);
+  }
+
+}
+
+function classToggle() {
+  const navs = document.querySelectorAll('.infor_Items');
+
+  navs.forEach(nav => nav.classList.toggle('infor_ToggleShow'));
+}
+
+
+
